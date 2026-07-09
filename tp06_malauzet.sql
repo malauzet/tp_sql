629038d0c5fe17ee6fb093ec8133697af2a28de2
malauzet <30903872+malauzet@users.noreply.github.com>
2026-07-09 14:51:57 +0200

    modif commentaires

--- tp06_malauzet.sql
+++ tp06_malauzet.sql
@@ -20,7 +20,8 @@ WHERE NOT EXISTS (
 	WHERE C.ID_ART = A.ID
 );
 
--- 3) d. Une erreur s’est glissée dans les commandes concernant Française d’imports. Les chiffres en base ne sont pas bons. Il faut doubler les quantités de tous les articles commandés à cette société.
+-- 3) d. Une erreur s’est glissée dans les commandes concernant Française d’imports.
+-- Les chiffres en base ne sont pas bons. Il faut doubler les quantités de tous les articles commandés à cette société.
 UPDATE COMPO C
 SET QTE = QTE * 2
 WHERE EXISTS (
@@ -30,7 +31,8 @@ WHERE EXISTS (
 	WHERE B.ID = C.ID_BON AND F.NOM = 'Française d''Imports'
 );
 
--- 3) e. Mettez au point une requête update qui permette de supprimer les éléments entre parenthèses dans les désignations. Il vous faudra utiliser des fonctions comme substring et position.
+-- 3) e. Mettez au point une requête update qui permette de supprimer les éléments entre parenthèses dans les désignations.
+-- Il vous faudra utiliser des fonctions comme substring et position.
 UPDATE ARTICLE
 SET DESIGNATION = SUBSTRING(DESIGNATION, 1, POSITION('(' IN DESIGNATION) - 1)
 WHERE DESIGNATION LIKE '%(%';
